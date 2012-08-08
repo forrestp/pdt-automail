8d1577a0e55065dc2a8c718fe96f4bba03ec86bc
Carlo Biedenharn <carlo.pwn@gmail.com>
2012-08-07 21:37:29 -0400

    updated gCrontab to write to an output file

--- generateCrontab.py
+++ generateCrontab.py
@@ -4,11 +4,13 @@ import datetime
 #               Parameters                  #
 #############################################
 
-inFile = 'testData.tsv'
+inFile  = 'testData.tsv'
 
 outFile = 'output.txt'
 
-format = "%m/%d/%Y"
+format  = "%m/%d/%Y"
+
+owner   = "username"
 
 hour =  8
 
@@ -18,12 +20,15 @@ def parseLine(line):
         d = datetime.datetime.strptime(a[0], format)
         out = "0 " + str(hour) + " " + str(d.day) + " " + str(d.month)
         out += " * python sendMail.py " + a[1] + "@mit.edu\n"
-        print out
+        return out
     except:
         #do nothing
         pass
 
 def run():
     f = open(inFile)
+    f2 = open(outFile, "w")
+    f2.write("PATH=/mit/%s/cron_scripts:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/X11R6/bin\n" % (owner) +
+             "MAILTO=\"%s@mit.edu\"\n" % (owner))
     for x in f:
-        parseLine(x)
+      f2.write(parseLine(x)+"\n")
